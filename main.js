6e6605ffde180fb231e320c4eb12670f75bbb04d
william <william-vivier@hotmail.fr>
2021-04-15 06:37:45 +0200

    test

--- main.js
+++ main.js
@@ -3,7 +3,11 @@ const app = express();
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/page.html', function (req, res) {
-    res.send('hello world' + req + res);
+    res.send('hello world');
+    res.send(res);
+    res.send(req);
+    res.send(JSON.parse(req));
+    res.send(JSON.parse(res));
 });
 app.listen(3000, () => {
     console.log("Started on PORT 3000");

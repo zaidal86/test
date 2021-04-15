d32ef430a0ce4ebf82ac43996ca160f1a2340829
william <william-vivier@hotmail.fr>
2021-04-15 06:35:16 +0200

    test

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
+app.get('/page.html', function (req, res) {
     res.send('hello world');
 });
 app.listen(3000, () => {
